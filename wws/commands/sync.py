963beafcbb1245cd1597d961ebb38c99ba267e67
Daniel Porto <daniel.porto@gmail.com>
2020-06-13 12:49:46 +0100

    Fix sync down

--- wws/commands/sync.py
+++ wws/commands/sync.py
@@ -88,49 +88,65 @@ class Sync:
             data = yaml.load(f, Loader=yaml.FullLoader)
 
         # filter only the selected aliases
-        if args["alias"]:
-            data = project(data, args['alias'])
+        if args['alias']:
+            data = [ d for d in data if any( [ a for a in args['alias'] if a.upper() in d['alias'].upper() ])]
 
-        # reverse source and destination
+        # synchronize warp points
         for item in data:
-            source = item['src']
-            item['src'] = item['dst'][0]
-            item['destination'] = [source] 
 
-        for item in data:
-            if path.exists(item['src']):
-                if args["verbose"]: # for safety always dry run
-                    print(f"synchronizing {item['src']}")
+            if not path.exists(item['src']):
+                if args['force']:
+                    print("Force creating local directory")
+                    if not args['dry_run']:
+                        pathlib.Path(item['src']).mkdir(parents=True, exist_ok=True)    
+                else:
+                    if args['debug'] or args['verbose']:
+                        print(f"Local path '{item['src']}' does not exits, skipping.")
+                    continue
 
-                for dst in item['dst']:
-                    # prevent creating remote before it exists ( if drive not mounted), 
-                    # we better use a command to force pushing to prevent errors
-                    # this approach need rework for remote locations (rsync over ssh)...
-                    if not path.exists(dst): 
-                        if args['debug'] or args['verbose']:
-                            print(f"Destination path '{dst}' does not exits, skipping.")
-                        continue
 
-                    params = []
-                    if args["dry_run"]:
-                        params.append("--dry-run")
+            if args['debug'] or args['verbose']:
+                print(f"synchronizing {item['src']}")
 
-                    # include global patterns
-                    for ex in args['exclude_patterns']:
-                        params.append(f"--exclude={ex}")
+            # select the first remote path
+            dst = item['dst'][0]
 
-                    # include per workspace options
-                    if item['opts']:
-                        params.extend(item['opts'])
+            # this approach need rework for remote locations (rsync over ssh)...
+            if not path.exists(dst): 
+                if args['debug'] or args['verbose']:
+                    print(f"Remote path '{dst}' does not exits, skipping.")
+                continue
 
-                    params.append(item['src'])
-                    params.append(dst)
-                    if args["verbose"]: # for safety always dry run
-                        print(f"\tto: {dst}")
-                    
-                    out = rsync[params].run() 
-                    if args["debug"] or args['dry_run']: 
-                        pprint(out) 
-            elif args['debug'] or args['verbose']:
-                print(f"Source path '{item['src']}' does not exits, skipping.") 
-    
+            params = []
+            if args["dry_run"]:
+                params.append("--dry-run")
+
+            # include global patterns
+            for ex in args['exclude_patterns']:
+                params.append(f"--exclude={ex}")
+
+            # include per workspace options
+            if item['opts']:
+                params.extend(item['opts'])
+
+            # fix source and destination
+            if item['src'][-1] is not '/':
+                item['src'] += '/'
+            if dst[-1] is not '/':
+                dst += '/'
+            
+
+            # finish with reversing direction remote -> local
+            params.append(dst)
+            params.append(item['src'])
+            if args['debug']:
+                pprint(params)
+
+            if args["verbose"]: # for safety always dry run
+                print(f"\tto: {dst}")
+            
+            out = rsync[params].run() 
+            if args["debug"] or args['dry_run']: 
+                pprint(out) 
+
+ 
